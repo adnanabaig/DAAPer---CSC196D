570302d80ed57b107f711b14944cccf5200c5c80
Pencilsharp333 <144190276+Pencilsharp333@users.noreply.github.com>
2024-11-23 20:02:42 -0800

    Cleanup redundant (#1)
    * move contracts and delete redundant directories
    
    * delete more
    
    * untrack abi
    
    ---------
    
    Co-authored-by: Pencilsharp333 <mattthewpabon@proton.me>

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
 //migrations/1_initial_migration.js
-const Migrations = artifacts.require("./Migrations.sol");
+const Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
